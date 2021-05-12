b6e1a10111b7756cb66bf0e7796f9b4ea7fcfe03
Bryan Eddy <v-bryan.eddy@humanaedge.com>
2021-05-12 08:14:36 -0700

    named options pattern

--- src/framework/Framework.PubSub/Extensions/ServiceCollectionExtensions.cs
+++ src/framework/Framework.PubSub/Extensions/ServiceCollectionExtensions.cs
@@ -106,7 +106,7 @@ namespace HumanaEdge.Webcore.Framework.PubSub.Extensions
             where TMessage : class
         {
             services.AddOptions();
-            services.Configure<PublisherOptions>(typeof(TMessage).FullName, configurationSection);
+            services.Configure<PublisherOptions>(typeof(TMessage).Name, configurationSection);
             services.AddSingleton<IPublisherClient<TMessage>, PublisherClient<TMessage>>();
             services.AddSingleton<IPublisherClientFactory, PublisherClientFactory>();
             services.AddSingleton<IPublishRequestConverter, PublishRequestConverter>();
